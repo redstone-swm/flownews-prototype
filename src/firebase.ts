66ef6a49efbf4d677a874265deddfe08736a4f0d
pgh <pggggggggh@yonsei.ac.kr>
2025-07-09 02:24:11 +0900

    feat: implement page tracking functionality

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,6 @@
 import {Capacitor} from '@capacitor/core';
 import {PushNotifications, type Token} from '@capacitor/push-notifications';
 import {Router} from "@tanstack/react-router";
-import {initializeApp} from 'firebase/app';
-import {getFirestore} from 'firebase/firestore';
-import {getAnalytics} from "@firebase/analytics";
 // @ts-ignore
 
 // 알림 설정
@@ -32,18 +29,3 @@ export function initializeFirebaseMessaging(router: Router) {
         }
     }).catch(e => console.error('Push notification action error:', e));
 }
-
-// firestore 설정
-const firebaseConfig = {
-    apiKey: "AIzaSyBqw_MV1rw9HrJJvs_dKLboUpfaIzGwcsQ",
-    authDomain: "sijeom-db66d.firebaseapp.com",
-    projectId: "sijeom-db66d",
-    storageBucket: "sijeom-db66d.firebasestorage.app",
-    messagingSenderId: "392894561750",
-    appId: "1:392894561750:web:ffad8aaad34dfca3157877",
-    measurementId: "G-FZB90NJQ3L"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
