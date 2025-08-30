856b068e50b42049a591d6ede29e8bdd95f9fd8a
sukkyun2 <goobghd@gmail.com>
2025-08-30 16:00:47 +0900

    chore: add models and hooks generated by orval

--- src/api/userApi.tsx
+++
@@ -1,7 +0,0 @@
-import axiosInstance from './axiosInstance';
-import type User from '@/types/user.ts';
-
-export const fetchUserMe = async (): Promise<User> => {
-    const response = await axiosInstance.get('/users/me');
-    return response.data;
-};
--- src/hooks/useSubscribeTopicMutation.ts
+++
@@ -1,8 +0,0 @@
-import {useMutation} from "@tanstack/react-query";
-import {subscribeTopic} from "@/api/topicApi.tsx";
-
-export function useSubscribeTopicMutation() {
-    return useMutation({
-        mutationFn: subscribeTopic
-    });
-}
