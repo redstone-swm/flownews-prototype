df2aead1739c4e145e5476ce80946f9c05986021
pgh <pggggggggh@yonsei.ac.kr>
2025-10-28 16:07:50 +0900

    fix: ensure app does not overlap navbar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,20 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'kr.sijeom',
-  appName: 'sijeom',
-  server: {
-    url: 'https://sijeom.kr',
-    cleartext: false
-  }
+    appId: 'kr.sijeom',
+    appName: 'sijeom',
+    server: {
+        url: 'https://sijeom.kr',
+        cleartext: false
+    },
+    plugins: {
+        StatusBar: {
+            overlaysWebView: false,
+            style: 'LIGHT',
+            backgroundColor: '#0B1021'
+        }
+    }
 };
+
 
 export default config;
