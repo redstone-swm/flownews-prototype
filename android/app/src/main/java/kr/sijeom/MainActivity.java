b526a729d6d2037f9bba57e56942977dc7562e43
sukkyun2 <goobghd@gmail.com>
2025-10-29 16:34:19 +0900

    fix: modify edge-to-edge rendering issue on Android

--- android/app/src/main/java/kr/sijeom/MainActivity.java
+++ android/app/src/main/java/kr/sijeom/MainActivity.java
@@ -1,5 +1,33 @@
 package kr.sijeom;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        
+        View decorView = getWindow().getDecorView();
+        ViewCompat.setOnApplyWindowInsetsListener(decorView, (v, insets) -> {
+            int topInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top;
+            int bottomInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
+            int leftInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).left;
+            int rightInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).right;
+            
+            View webView = findViewById(android.R.id.content);
+            if (webView != null) {
+                webView.setPadding(leftInset, topInset, rightInset, bottomInset);
+            }
+            
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
+}
